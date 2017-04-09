b782c1a5f1af4dbecdbfc251baa3919679692b5e
Anupam-dagar <dagaranupam@gmail.com>
2017-04-09 22:42:05 +0530

    Changed animation to puffIn

--- js/animate.js
+++ js/animate.js
@@ -4,100 +4,100 @@ function removeIt(obj, className) {
     }, 2000);
 }
 		$('#tile_0').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_1').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_2').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_3').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_4').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_5').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_6').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_7').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_8').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_9').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_10').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_11').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_12').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_13').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_14').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_15').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_16').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_17').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_18').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_19').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_20').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_21').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_22').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		$('#tile_23').click(function () {
-  $(this).addClass('magictime vanishIn');
-  removeIt(this, 'magictime vanishIn');
+  $(this).addClass('magictime puffIn');
+  removeIt(this, 'magictime puffIn');
 });
 		setInterval(function(){ 
 	$('#moves').toggleClass('magictime puffIn');
